c3bc79f03b05e5de359928151de8a9395b8f6f56
ngocson07042004 <tranthai16102004@gmail.com>
2025-02-13 10:11:04 +0700

    update data user

--- src/data/dataUser.js
+++ src/data/dataUser.js
@@ -1,5 +1,6 @@
 const dataUser = [
-    {id: 1, username: "admin", password: "admin", corfimPassword: "admin"}
+    {id: 1, username: "admin", password: "admin", corfimPassword: "admin"},
+    {id: 2, username: "client", password: "client", corfimPassword: "client"}
 ]
 
 export default dataUser
